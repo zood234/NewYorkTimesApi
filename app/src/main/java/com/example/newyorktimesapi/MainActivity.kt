bfdddf6fbc79b7ff91bc698a2a6f7f14fdefd12f
zood234 <zainmahmood@live.co.uk>
2021-07-14 19:24:41 +0100

    Added Recycler View App Very Messy Clean up first

--- app/src/main/java/com/example/newyorktimesapi/MainActivity.kt
+++ app/src/main/java/com/example/newyorktimesapi/MainActivity.kt
@@ -4,22 +4,60 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsappwithapi.NewsApi
 import com.example.newsappwithapi.dataWeb.NewsResponse
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+private lateinit var todoAdapter: TodoAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoAdapter: TodoAdapter
     private var newsData: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         newsData = findViewById(R.id.textView)
         findViewById<View>(R.id.button).setOnClickListener { getCurrentData() }
+
+
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
 
     internal fun getCurrentData() {
         val retrofit = Retrofit.Builder()
---
+++ app/src/main/java/com/example/newyorktimesapi/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.newyorktimesapi
+
+data class Todo(
+    val title: String,
+    var isChecked: Boolean = false
+)
